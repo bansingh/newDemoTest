9ffae3196b2d968996d4668bb4ba61bfb22ee60d
vanda <vandana002@gmail.com>
2022-05-19 13:38:16 -0400

    first commit

--- GitPushTest/src/GitPush.java
+++ GitPushTest/src/GitPush.java
@@ -3,7 +3,7 @@ public class GitPush {
 
 	public static void main(String args[]) {
 		
-		System.out.println("This is my new token");
+		System.out.println("This is my token");
 		System.out.println("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");
 	}
 }
